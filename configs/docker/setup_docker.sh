d7870212efcd42691c57625a253206ea66e5be12
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2023-07-07 23:06:19 -0700

    docker changes

--- configs/docker/setup_docker.sh
+++ configs/docker/setup_docker.sh
@@ -6,9 +6,12 @@ cp -r ~/.localrc  ~/.dot-files .
 cp -r ~/.ssh .
 
 echo "copying latest docker files"
-cp /home/pooya/proj/templates/Dockerfile .
-cp /home/pooya/proj/templates/make_container.sh .
-cp /home/pooya/proj/templates/build_image.sh .
+# cp /home/pooya/proj/templates/Dockerfile .
+# cp /home/pooya/proj/templates/make_container.sh .
+# cp /home/pooya/proj/templates/build_image.sh .
+cp `dirname $0`/Dockerfile .
+cp `dirname $0`/make_container.sh .
+cp `dirname $0`/build_image.sh .
 
 echo "copying done. edit the build_image and make_container files accordingly"
 mkdir -p ./data
