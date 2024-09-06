d408b878882ad5bb2b60f0d5f8e1a02d75219911
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2024-09-05 18:01:57 -0700

    disable a plugin

--- configs/nvim/lua/plugins/disabled.lua
+++ configs/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- disable trouble
   -- { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
