7cffe329ac5203e29cdd25b4eee54ec5e9ea74d8
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2024-09-05 18:01:48 -0700

    copilot

--- configs/nvim/lua/plugins/copilot.lua
+++ configs/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
-  --   {
-  --     "L3MON4D3/LuaSnip",
-  --     keys = function()
-  --       return {}
-  --     end,
-  --   },
-  --   {
-  --     "github/copilot.vim",
-  --   },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "github/copilot.vim",
+  },
 }
