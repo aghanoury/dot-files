c0b917f91871c83727514c0cfc66d97242fe3392
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2024-09-05 18:01:40 -0700

    fix copilot plus some other copy/paste options

--- configs/nvim/lua/config/options.lua
+++ configs/nvim/lua/config/options.lua
@@ -14,3 +14,43 @@
 vim.opt.wrap = true -- Disable line wrap
 vim.opt.linebreak = true -- Wrap on word boundary
 vim.opt.conceallevel = 0
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--   },
+--   -- paste = {
+--   --   ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+--   --   ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+--   -- },
+-- }
+--
+-- -- vim.g.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
+--
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
